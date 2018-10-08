5eeeca347bc8562a2a6e00cc33965213865c5e9e
Bruno Cardoso Ambrosio <39098765+BrunoCA-xD@users.noreply.github.com>
2018-10-08 19:33:44 -0300

    remove

--- src/main/java/bca/internshipsolutions/TwigNews/repository/TreesRepo.java
+++
@@ -1,9 +0,0 @@
-package bca.internshipsolutions.TwigNews.repository;
-
-
-import bca.internshipsolutions.TwigNews.model.Tree;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TreesRepo  extends JpaRepository<Tree, Integer>{
-
-}
